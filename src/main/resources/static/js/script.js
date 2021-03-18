07ef76bde66f4e12392a6060d62e2a3720cb9829
Edivaldo Ferreira De Souza Júnior <57267120+Edi6758@users.noreply.github.com>
2021-03-18 15:53:09 -0300

    Atualização das telas no projeto

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -5,7 +5,6 @@ const showModalDeletar = function(id) {
 
 const deletar = function(path) {
     let id = $('#id_deletar').val()
-    console.log("edi" + id)
     if (id !== 0) {
         window.location.href = '/' + path + '/delete/' + id
     }
